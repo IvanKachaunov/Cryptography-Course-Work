32a6b1324f9e6ca6629bece4b8ae9b879872499f
Ivan Kachaunov <ivan.kachaunov1@gmail.com>
2022-05-15 18:14:19 +0300

    Cryptography-Course-Work

---
+++ app.js
@@ -0,0 +1,27 @@
+let encryptBtn = document.getElementById('encrypt-btn');
+let eInput = document.getElementById('encryptedInput-1');
+let pInput = document.getElementById('plainInput');
+let inputs = [eInput, pInput];
+
+inputs.forEach(input => {
+    input.oninput = () => {
+        input.value = input.value.toUpperCase()
+    }
+})
+
+function encrypt(){
+    let pInput = document.getElementById('plainInput').value;
+    let solved = '';
+    let shiftInput = parseInt(document.getElementById('shiftInput').value) 
+
+    for(let i = 0; i < pInput.length; i++){
+        let ascii_num = pInput[i].charCodeAt()
+        let sum = ascii_num + shiftInput;
+        sum >= 65 && sum <= 90 ? solved += String.fromCharCode(sum) : sum > 90 ? solved += String.fromCharCode(65 + (sum & 91)) : solved += pInput[i]
+    }
+
+    eInput.value = solved;
+
+}
+
+encryptBtn.addEventListener('click', encrypt);
